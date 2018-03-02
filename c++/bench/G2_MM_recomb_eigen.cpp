a9dbb18bade82385d8121daea20bbb89454457c0
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-03-02 10:03:11 -0500

    [g2] eigen test

--- c++/bench/G2_MM_recomb_eigen.cpp
+++ c++/bench/G2_MM_recomb_eigen.cpp
@@ -1,5 +1,7 @@
 
-#define EIGEN_USE_BLAS
+//#define EIGEN_USE_BLAS
+//#define EIGEN_ENABLE_AVX512
+//#define EIGEN_VECTORIZE_AVX512
 
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
@@ -78,9 +80,9 @@ static void G2_MM_ph_w0_last_term_Eigen_tensor_map(benchmark::State &state) {
 
 static void G2_MM_ph_w0_last_term_Eigen_tensor_rowmajor(benchmark::State &state) {
 
-  Eigen::Tensor<value_t, 4> M1(o, o, n, n);
-  Eigen::Tensor<value_t, 4> M2(o, o, n, n);
-  Eigen::Tensor<value_t, 6> g2(o, o, o, o, n, n);
+  Eigen::Tensor<value_t, 4, Eigen::RowMajor> M1(o, o, n, n);
+  Eigen::Tensor<value_t, 4, Eigen::RowMajor> M2(o, o, n, n);
+  Eigen::Tensor<value_t, 6, Eigen::RowMajor> g2(o, o, o, o, n, n);
 
   for (auto _ : state) {
 
